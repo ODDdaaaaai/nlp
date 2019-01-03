cbfb89f057f70a3f82435589fa9ebbb7528b4996
Odai <odaimohammeds@gmail.com>
2019-01-03 21:35:25 +0200

    moved search locations to Event model added more Events pickled tweets

--- model/Event.py
+++ model/Event.py
@@ -1,5 +1,6 @@
 class Event:
-    def __init__(self, search_queries, since, until):
+    def __init__(self, search_queries, since, until, near):
         self.search_queries = search_queries
         self.since = since
         self.until = until
+        self.near = near
