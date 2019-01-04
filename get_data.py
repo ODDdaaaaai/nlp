8c0c818197509b24abf6bd57dd5c5c9b51e79c5d
Odai <odaimohammeds@gmail.com>
2019-01-04 23:06:49 +0200

    code and project structure improvements, modified .gitignore

--- get_data.py
+++ get_data.py
@@ -57,10 +57,12 @@ for category_index, category in enumerate(EVENTS):
                 print('Fetched ' + str(len(search_query_fetched_tweets)) + ' new tweets successfully')
                 print('Fetched ' + str(len(fetched_tweets)) + ' total tweets\n')
 
+                part_file_id = str(category_index) + str(event_index) + str(search_query_index) + str(near_index)
+
                 # Save fetched tweets
                 pickle_data(fetched_tweets, DATA_BASE_DIRECTORY + 'fetched_data.pckl')
                 pickle_data(search_query_fetched_tweets,
-                            PART_DATA_BASE_DIRECTORY + str(search_query_index) + str(near_index) + '/fetched_data.pckl')
+                            PART_DATA_BASE_DIRECTORY + part_file_id + '/fetched_data.pckl')
 
                 # Create list of custom model
                 search_query_tweets = []
@@ -72,9 +74,9 @@ for category_index, category in enumerate(EVENTS):
                 # Save custom tweets
                 pickle_data(tweets, DATA_BASE_DIRECTORY + 'data.pckl')
                 pickle_data(search_query_tweets,
-                            PART_DATA_BASE_DIRECTORY + str(search_query_index) + str(near_index) + '/data.pckl')
+                            PART_DATA_BASE_DIRECTORY + part_file_id + '/data.pckl')
 
                 # Save tweets as JSON
                 save_json(tweets, DATA_BASE_DIRECTORY + 'data.json')
                 save_json(search_query_tweets,
-                          PART_DATA_BASE_DIRECTORY + str(search_query_index) + str(near_index) + '/data.json')
+                          PART_DATA_BASE_DIRECTORY + part_file_id + '/data.json')
