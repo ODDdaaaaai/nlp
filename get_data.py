f9598e2c0fd20dfb4d1f5de7f38654ff4d304f3e
Odai <odaimohammeds@gmail.com>
2019-01-04 18:30:08 +0200

    made saving of data incremental, code structure improvements

--- get_data.py
+++ get_data.py
@@ -1,116 +1,72 @@
-# coding=utf-8
 import codecs
 import json
+import os
 import pickle
 
 import got
 
-from model.Event import Event
+from data_source.data_source import DATA_BASE_DIRECTORY
+from data_source.data_source import EVENTS
+from data_source.data_source import PART_DATA_BASE_DIRECTORY
 from model.Tweet import Tweet
 
 # Search criteria
 TOP_TWEETS = True
-MAX_TWEETS = 10
+MAX_TWEETS = 5000
 WITHIN = '1000mi'
 
-# Events to search for
-HAPPY_EVENTS = [
-    Event(['فطر', 'عيد', 'سعيد', 'كل عام و أنتم بخير'], '2016-7-5', '2016-7-8', ['مصر', 'السعودية']),
-    Event(['الفطر', 'عيد', 'سعيد', 'كل عام و أنتم بخير'], '2017-6-24', '2017-6-27', ['مصر', 'السعودية']),
-    Event(['فطر', 'عيد', 'سعيد', 'كل عام و أنتم بخير'], '2018-6-14', '2018-6-16', ['مصر', 'السعودية']),
 
-    Event(['الأضحى', 'عيد', 'سعيد', 'كل عام و أنتم بخير'], '2016-9-11', '2016-9-15', ['مصر', 'السعودية']),
-    Event(['الأضحى', 'عيد', 'سعيد', 'كل عام و أنتم بخير'], '2017-8-31', '2018-9-4', ['مصر', 'السعودية']),
-    Event(['الأضحى', 'عيد', 'سعيد', 'كل عام و أنتم بخير'], '2018-8-20', '2018-8-24', ['مصر', 'السعودية']),
+def pickle_data(data_list, file_path):
+    part_file_directory = os.path.dirname(file_path)
+    if not os.path.exists(part_file_directory):
+        os.makedirs(part_file_directory)
+    pickle.dump(data_list, open(file_path, 'wb'))
 
-    Event(['المولد', 'كل عام وانتم بخير'], '2016-12-10', '2016-12-10', ['مصر', 'السعودية']),
-    Event(['المولد', 'كل عام وانتم بخير'], '2017-11-30', '2017-12-1', ['مصر', 'السعودية']),
-    Event(['المولد', 'كل عام وانتم بخير'], '2018-11-19', '2018-11-21', ['مصر', 'السعودية']),
 
-    Event(['السنة الهجرية', 'كل عام وانتم بخير'], '2016-10-1', '2016-10-1', ['مصر', 'السعودية']),
-    Event(['السنة الهجرية', 'كل عام وانتم بخير'], '2017-9-20', '2017-9-21', ['مصر', 'السعودية']),
-    Event(['السنة الهجرية', 'كل عام وانتم بخير'], '2018-9-10', '2018-9-11', ['مصر', 'السعودية']),
+def save_json(data_list, file_path):
+    part_file_directory = os.path.dirname(file_path)
+    if not os.path.exists(part_file_directory):
+        os.makedirs(part_file_directory)
+    with codecs.open(file_path, 'w', encoding='utf-8') as outfile:
+        json.dump([element.__dict__ for element in data_list], outfile, indent=4, ensure_ascii=False)
 
-    Event(['المسيح', 'كريسمس', 'عيد مجيد', 'ميلاد', "سعيد"], '2016-12-24', '2016-12-25', ['مصر', 'السعودية']),
-    Event(['المسيح', 'كريسمس', 'عيد مجيد', 'ميلاد', "سعيد"], '2017-12-24', '2017-12-25', ['مصر', 'السعودية']),
-    Event(['المسيح', 'كريسمس', 'عيد مجيد', 'ميلاد', "سعيد"], '2018-12-24', '2018-12-25', ['مصر', 'السعودية']),
+    outfile.close()
 
-    Event(['رأس السنة', 'كل عام و أنتم بخير'], '2016-12-31', '2017-1-1', ['مصر', 'السعودية']),
-    Event(['رأس السنة', 'كل عام و أنتم بخير'], '2017-12-31', '2018-1-1', ['مصر', 'السعودية']),
-    Event(['رأس السنة', 'كل عام و أنتم بخير'], '2018-12-31', '2019-1-1', ['مصر', 'السعودية']),
 
-    Event(['الأم'], '2016-03-21', '2016-03-22', ['مصر', 'السعودية']),
-    Event(['الأم'], '2017-03-21', '2017-03-22', ['مصر', 'السعودية']),
-    Event(['الأم'], '2018-03-21', '2018-03-22', ['مصر', 'السعودية']),
-]
-
-ANGRY_EVENTS = [
-    Event(['ميدان التحرير', 'غضب', 'ثورة'], '2011-1-25', '2011-1-28', ['مصر']),
-    Event(['ميدان التحرير', 'غضب', 'ثورة'], '2011-1-28', '2011-1-31', ['مصر']),
-    Event(['ميدان التحرير', 'ثورة'], '2011-2-1', '2011-2-10', ['مصر']),
-    Event(['ميدان التحرير', 'جمعة الزحف', 'ثورة', 'تنحي'], '2011-2-11', '2011-2-12', ['مصر']),
-    Event(['ميدان التحرير', 'ثورة'], '2011-2-13', '2011-2-24', ['مصر']),
-    Event(['ميدان التحرير', 'ثورة', 'محاكمة'], '2011-2-25', '2011-2-24', ['مصر']),
-    Event(['عاصفة الحزم', 'آل سعود', 'حوثيون', '13 مدنيً'], '2015-03-25', '2018-03-26', ['السعودية']),
-    Event(['مأرب', 'غارة', 'ضربة', 'صنعاء', 'مخيم المرزق', 'أطفال', 'نساء'], '2015-03-26', '2018-03-31', ['السعودية']),
-    Event(['الظهرة', 'مقتل', 'شهيد', 'انسان'], '2015-04-3', '2018-04-19', ['السعودية']),
-    Event(['نقيل سُمارة', 'مقتل', 'شهيد', 'انسان', 'مدني'], '2015-04-20', '2018-04-21', ['السعودية']),
-    Event(['طفل', 'مقتل', 'نزوح', 'انسان', 'مدني'], '2015-04-22', '2018-04-25', ['السعودية']),
-    Event(['طفل', 'مقتل', 'نزوح', 'انسان', 'مدني'], '2015-04-26', '2018-04-30', ['السعودية']),
-]
-
-SAD_EVENTS = [
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2017-1-1', '2018-2-28', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2017-3-1', '2018-4-30', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2017-5-1', '2018-6-30', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2017-7-1', '2018-8-31', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2017-9-1', '2018-10-31', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2017-11-1', '2018-12-31', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2018-1-1', '2018-2-28', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2018-3-1', '2018-4-30', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2018-5-1', '2018-6-30', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2018-7-1', '2018-8-31', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2018-9-1', '2018-10-31', ['مصر', 'السعودية']),
-    Event(['الله يرحمك', 'انا_لله_وانا_اليه_راجعون', 'حزين', 'رسبت'], '2018-11-1', '2018-12-31', ['مصر', 'السعودية']),
-]
-
-# All events
-EVENTS = [
-    HAPPY_EVENTS
-]
-
-# List of tweets
+# List of fetched tweets
 fetched_tweets = []
 
 # Get tweets
-for category in EVENTS:
-    for event in category:
+for category_index, category in enumerate(EVENTS):
+    for event_index, event in enumerate(category):
         for search_query in event.search_queries:
             for near in event.near:
+                print('Fetching: category: ' + str(category_index) + ' Event: ' + str(
+                    event_index) + ' Search query: ' + search_query + ' Near: ' + near)
+
                 tweetCriteria = got.manager.TweetCriteria().setTopTweets(TOP_TWEETS).setSince(event.since).setUntil(
                     event.until).setNear(near).setWithin(WITHIN).setQuerySearch(search_query).setMaxTweets(MAX_TWEETS)
-                fetched_tweets += got.manager.TweetManager.getTweets(tweetCriteria)
+                data = got.manager.TweetManager.getTweets(tweetCriteria)
 
-# Save fetched tweets
-pickle.dump(fetched_tweets, open('data/fetched_data.pckl', 'wb'))
+                fetched_tweets += data
 
-# Create list of custom model
-tweets = []
+                print('Fetched ' + str(len(data)) + ' new tweets successfully')
+                print('Fetched ' + str(len(fetched_tweets)) + ' total tweets\n')
 
-for tweet in fetched_tweets:
-    tweets.append(Tweet(tweet.text, tweet.hashtags, [], []))
+                # Save fetched tweets
+                pickle_data(fetched_tweets, DATA_BASE_DIRECTORY + 'fetched_data.pckl')
+                pickle_data(fetched_tweets, PART_DATA_BASE_DIRECTORY + str(event_index) + '/fetched_data.pckl')
 
-# Save custom tweets
-pickle.dump(tweets, open('data/data.pckl', 'wb'))
+                # Create list of custom model
+                tweets = []
 
-# Save tweets as JSON
-with codecs.open('data/data.json', 'w', encoding='utf-8') as outfile:
-    data = json.dump([tweet.__dict__ for tweet in tweets], outfile, indent=4, ensure_ascii=False)
+                for tweet in fetched_tweets:
+                    tweets.append(Tweet(tweet.text, tweet.hashtags, [], []))
 
-outfile.close()
+                # Save custom tweets
+                pickle_data(tweets, DATA_BASE_DIRECTORY + 'data.pckl')
+                pickle_data(tweets, PART_DATA_BASE_DIRECTORY + str(event_index) + '/data.pckl')
 
-# Print fetched tweets
-count = 1
-for tweet in tweets:
-    print(str(count) + tweet.text)
-    count += 1
+                # Save tweets as JSON
+                save_json(tweets, DATA_BASE_DIRECTORY + 'data.json')
+                save_json(tweets, PART_DATA_BASE_DIRECTORY + str(event_index) + '/data.json')
