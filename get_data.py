cece4eab4fb5bcce1c6d84f36722fefd4bd8248c
Odai <odaimohammeds@gmail.com>
2019-01-04 20:35:11 +0200

    fixed problems with tweet collection, Fetched 23840 total tweets

--- get_data.py
+++ get_data.py
@@ -16,19 +16,19 @@ MAX_TWEETS = 5000
 WITHIN = '1000mi'
 
 
-def pickle_data(data_list, file_path):
+def pickle_data(data, file_path):
     part_file_directory = os.path.dirname(file_path)
     if not os.path.exists(part_file_directory):
         os.makedirs(part_file_directory)
-    pickle.dump(data_list, open(file_path, 'wb'))
+    pickle.dump(data, open(file_path, 'wb'))
 
 
-def save_json(data_list, file_path):
+def save_json(data, file_path):
     part_file_directory = os.path.dirname(file_path)
     if not os.path.exists(part_file_directory):
         os.makedirs(part_file_directory)
     with codecs.open(file_path, 'w', encoding='utf-8') as outfile:
-        json.dump([element.__dict__ for element in data_list], outfile, indent=4, ensure_ascii=False)
+        json.dump([element.__dict__ for element in data], outfile, indent=4, ensure_ascii=False)
 
     outfile.close()
 
@@ -36,37 +36,45 @@ def save_json(data_list, file_path):
 # List of fetched tweets
 fetched_tweets = []
 
+tweets = []
+
 # Get tweets
 for category_index, category in enumerate(EVENTS):
     for event_index, event in enumerate(category):
-        for search_query in event.search_queries:
-            for near in event.near:
+        for search_query_index, search_query in enumerate(event.search_queries):
+            for near_index, near in enumerate(event.near):
                 print('Fetching: category: ' + str(category_index) + ' Event: ' + str(
                     event_index) + ' Search query: ' + search_query + ' Near: ' + near)
 
+                search_query_fetched_tweets = []
+
                 tweetCriteria = got.manager.TweetCriteria().setTopTweets(TOP_TWEETS).setSince(event.since).setUntil(
                     event.until).setNear(near).setWithin(WITHIN).setQuerySearch(search_query).setMaxTweets(MAX_TWEETS)
-                data = got.manager.TweetManager.getTweets(tweetCriteria)
+                search_query_fetched_tweets += got.manager.TweetManager.getTweets(tweetCriteria)
 
-                fetched_tweets += data
+                fetched_tweets += search_query_fetched_tweets
 
-                print('Fetched ' + str(len(data)) + ' new tweets successfully')
+                print('Fetched ' + str(len(search_query_fetched_tweets)) + ' new tweets successfully')
                 print('Fetched ' + str(len(fetched_tweets)) + ' total tweets\n')
 
                 # Save fetched tweets
                 pickle_data(fetched_tweets, DATA_BASE_DIRECTORY + 'fetched_data.pckl')
-                pickle_data(fetched_tweets, PART_DATA_BASE_DIRECTORY + str(event_index) + '/fetched_data.pckl')
+                pickle_data(search_query_fetched_tweets,
+                            PART_DATA_BASE_DIRECTORY + str(search_query_index) + str(near_index) + '/fetched_data.pckl')
 
                 # Create list of custom model
-                tweets = []
+                search_query_tweets = []
 
-                for tweet in fetched_tweets:
-                    tweets.append(Tweet(tweet.text, tweet.hashtags, [], []))
+                for tweet in search_query_fetched_tweets:
+                    search_query_tweets.append(Tweet(tweet.text, tweet.hashtags, [], []))
 
+                tweets += search_query_tweets
                 # Save custom tweets
                 pickle_data(tweets, DATA_BASE_DIRECTORY + 'data.pckl')
-                pickle_data(tweets, PART_DATA_BASE_DIRECTORY + str(event_index) + '/data.pckl')
+                pickle_data(search_query_tweets,
+                            PART_DATA_BASE_DIRECTORY + str(search_query_index) + str(near_index) + '/data.pckl')
 
                 # Save tweets as JSON
                 save_json(tweets, DATA_BASE_DIRECTORY + 'data.json')
-                save_json(tweets, PART_DATA_BASE_DIRECTORY + str(event_index) + '/data.json')
+                save_json(search_query_tweets,
+                          PART_DATA_BASE_DIRECTORY + str(search_query_index) + str(near_index) + '/data.json')
